fb77403d281eaf918c573201419ae77da702aa52
blm <mcleanb@photo-nomad.net>
2022-07-26 13:48:03 -0400

    add another search test case.

--- src/transforms/wordSearch.test.js
+++ src/transforms/wordSearch.test.js
@@ -2,80 +2,151 @@ import wordSearch from './wordSearch';
 import perfWrapper from './__data__/perfWrapper';
 
 describe('test wordSearch', () => {
-  const perf = perfWrapper({
-    blocks: [
-      {
-        "type": "paragraph",
-        "subtype": "usfm:p",
-        "content": [
-          {
-            "type": "mark",
-            "subtype": "chapter",
-            "atts": {
-              "number": "1"
-            }
-          },
-          {
-            "type": "mark",
-            "subtype": "verses",
-            "atts": {
-              "number": "1"
-            }
-          },
-          "I",
-          ", ",
-          "Paul",
-          ", ",
-        ]
-      },
-    ]
-  });
-
   test('ignore case succeeds', () => {
+    const perf = perfWrapper({
+      blocks: [
+        {
+          "type": "paragraph",
+          "subtype": "usfm:p",
+          "content": [
+            {
+              "type": "mark",
+              "subtype": "chapter",
+              "atts": {
+                "number": "1"
+              }
+            },
+            {
+              "type": "mark",
+              "subtype": "verses",
+              "atts": {
+                "number": "1"
+              }
+            },
+            "I",
+            ", ",
+            "Paul",
+            ", ",
+          ]
+        },
+      ]
+    });
+
     const searchString = 'paul';
     const ignoreCase = '1';
 
-    const { matches } = wordSearch.code({perf, searchString, ignoreCase})
+    const {matches} = wordSearch.code({perf, searchString, ignoreCase})
 
     expect(matches).toEqual([
-        {
-            "chapter": "1",
-            "verses": "1",
-            "content": [
-                "I",
-                ", ",
-                {
-                    type: "wrapper",
-                    subtype: "x-search-match",
-                    content: [
-                        'Paul'
-                    ]
-                },
-            ],
-       },
+      {
+        "chapter": "1",
+        "verses": "1",
+        "content": [
+          "I",
+          ", ",
+          {
+            type: "wrapper",
+            subtype: "x-search-match",
+            content: [
+              'Paul'
+            ]
+          },
+        ],
+      },
     ]);
+  });
 
   test('case sensitive fails on case mismatch', () => {
+    const perf = perfWrapper({
+      blocks: [
+        {
+          "type": "paragraph",
+          "subtype": "usfm:p",
+          "content": [
+            {
+              "type": "mark",
+              "subtype": "chapter",
+              "atts": {
+                "number": "1"
+              }
+            },
+            {
+              "type": "mark",
+              "subtype": "verses",
+              "atts": {
+                "number": "1"
+              }
+            },
+            "I",
+            ", ",
+            "Paul",
+            ", ",
+          ]
+        },
+      ]
+    });
+
     const searchString = 'paul';
     const ignoreCase = '0';
 
-    const {matches: matchesCS} = wordSearch.code({perf, searchString, ignoreCase})
+    const {matches} = wordSearch.code({perf, searchString, ignoreCase})
 
-    expect(matchesCS).toEqual(
+    expect(matches).toEqual(
       []
     );
   });
 
   test('case sensitive succeeds', () => {
+    const perf = perfWrapper({
+      blocks: [
+        {
+          "type": "paragraph",
+          "subtype": "usfm:p",
+          "content": [
+            {
+              "type": "mark",
+              "subtype": "chapter",
+              "atts": {
+                "number": "1"
+              }
+            },
+            {
+              "type": "mark",
+              "subtype": "verses",
+              "atts": {
+                "number": "1"
+              }
+            },
+            "I",
+            ", ",
+            "Paul",
+            ", ",
+          ]
+        },
+      ]
+    });
+
     const searchString = 'Paul';
     const ignoreCase = '0';
 
-    const {matches: matchesCS} = wordSearch.code({perf, searchString, ignoreCase})
+    const {matches} = wordSearch.code({perf, searchString, ignoreCase})
 
-    expect(matchesCS).toEqual(
-      [
-        '1:1',
-      ]
-    );
+    expect(matches).toEqual([
+      {
+        "chapter": "1",
+        "verses": "1",
+        "content": [
+          "I",
+          ", ",
+          {
+            type: "wrapper",
+            subtype: "x-search-match",
+            content: [
+              'Paul'
+            ]
+          },
+        ],
+      },
+    ]);
   });
 });
