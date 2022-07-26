8214f6adf9bb959683af3f856244c9d28c94dd97
blm <mcleanb@photo-nomad.net>
2022-07-26 13:24:41 -0400

    tweak search tests

--- src/transforms/wordSearch.test.js
+++ src/transforms/wordSearch.test.js
@@ -1,48 +1,70 @@
 import wordSearch from './wordSearch';
 import perfWrapper from './__data__/perfWrapper';
 
-test('wordSearch finds some words', () => {
-    const perf = perfWrapper({blocks:[
-        {
-            "type": "paragraph",
-            "subtype": "usfm:p",
-            "content": [
-                {
-                    "type": "mark",
-                    "subtype": "chapter",
-                    "atts": {
-                        "number": "1"
-                    }
-                },
-                {
-                    "type": "mark",
-                    "subtype": "verses",
-                    "atts": {
-                        "number": "1"
-                    }
-                },
-                "I",
-                ", ",
-                "Paul",
-                ", ",
-            ]
-        },
-    ]});
+describe('test wordSearch', () => {
+  const perf = perfWrapper({
+    blocks: [
+      {
+        "type": "paragraph",
+        "subtype": "usfm:p",
+        "content": [
+          {
+            "type": "mark",
+            "subtype": "chapter",
+            "atts": {
+              "number": "1"
+            }
+          },
+          {
+            "type": "mark",
+            "subtype": "verses",
+            "atts": {
+              "number": "1"
+            }
+          },
+          "I",
+          ", ",
+          "Paul",
+          ", ",
+        ]
+      },
+    ]
+  });
 
+  test('ignore case succeeds', () => {
     const searchString = 'paul';
     const ignoreCase = '1';
 
-    const { matches } = wordSearch.code({perf, searchString, ignoreCase})
+    const {matches} = wordSearch.code({perf, searchString, ignoreCase})
 
     expect(matches).toEqual(
-        [
-            '1:1',
-        ]
+      [
+        '1:1',
+      ]
     );
+  });
 
-    const { matches: matchesCS } = wordSearch.code({perf, searchString, ignoreCase: '0'})
+  test('case sensitive fails on case mismatch', () => {
+    const searchString = 'paul';
+    const ignoreCase = '0';
+
+    const {matches: matchesCS} = wordSearch.code({perf, searchString, ignoreCase})
 
     expect(matchesCS).toEqual(
-        []
+      []
     );
+  });
+
+  test('case sensitive succeeds', () => {
+    const searchString = 'Paul';
+    const ignoreCase = '0';
+
+    const {matches: matchesCS} = wordSearch.code({perf, searchString, ignoreCase})
+
+    expect(matchesCS).toEqual(
+      [
+        '1:1',
+      ]
+    );
+  });
 });
