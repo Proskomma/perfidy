af6c324c0c9e3a938322f35a0ddb6e0e0d8b3c1a
David Stone <david@nnucomputerwhiz.com>
2022-07-27 16:52:15 -0600

    Add searchWord Pipeline Integration test

---
+++ src/__tests__/searchWordPipeline.js
@@ -0,0 +1,39 @@
+import pipelineJson from '../../pipelines/searchWordPipeline.json';
+import runCallback from '../lib/runCallback';
+import stepTemplates from "../lib/stepTemplates";
+import {Proskomma}  from 'proskomma';
+
+describe('wordSearch pipeline integration test', () => {
+    test('run', async () => {
+        // Use proskomma directly instead of with react hook.
+        const proskomma = new Proskomma();
+        // We need to tweak this when not using hook. Not sure why.
+        pipelineJson[1].localValue = "{\"lang\": \"eng\", \"abbr\": \"ust\"}";
+
+        pipelineJson
+            .filter(st => st.type === "Transform")
+            .forEach(
+                st => {
+                    st.code = stepTemplates.Transform[st.name].code;
+                    st.description = stepTemplates.Transform[st.name].description;
+                }
+            );
+        const specSteps = pipelineJson;
+
+        let results;
+        const setResults = (value) => {
+            results = value;
+        };
+        const setRunIssues = () => {};
+
+        await runCallback({
+            specSteps,
+            setResults,
+            setRunIssues,
+            proskomma
+        })
+
+        expect(results[2].value).toContainEqual({"chapter": "3", "content": ["God", " ", "generously", " ", "gave", " ", "us", " ", "his", " ", "Holy", " ", "Spirit", " ", "when", " ", {"content": ["Jesus"], "subtype": "x-search-match", "type": "wrapper"}, " ", "the", " ", "Messiah", " ", "saved", " ", "us", ". "], "verses": "6"});
+    });
+});
+
