ffc8111bc3c47f5358f9a12f554a1780ef5e66be
Abel Perez <abelper54@gmail.com>
2022-07-28 11:15:52 -0400

    Removed > symbol from wordWrap button

--- src/components/EditorWrapper.js
+++ src/components/EditorWrapper.js
@@ -73,7 +73,7 @@ function EditorWrapper({ results }) {
                   }))
                 }
               >
-                {"W>"}
+                {"W"}
               </button>
             </span>
           </>
