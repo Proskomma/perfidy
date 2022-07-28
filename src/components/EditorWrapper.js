3bd294c279d43dfc81d76b9328b9c4b520e7c7ab
Abel Perez <abelper54@gmail.com>
2022-07-28 05:28:11 -0400

    Editor fixes

--- src/components/EditorWrapper.js
+++ src/components/EditorWrapper.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Editor from "@monaco-editor/react";
 
+const cleanEditorContent = {
+    title: "index",
+    value: "",
+    inputType: "json"
+}
+
 function EditorWrapper({ results }) {
-    const [editorContent, setEditorContent] = useState({});
+    const [editorContent, setEditorContent] = useState(cleanEditorContent);
     const editorRef = useRef(null);
 
     useEffect(() => {
+        console.log("updated editor");
         editorRef.current?.focus();
     }, [editorContent?.id]);
 
@@ -13,38 +20,61 @@ function EditorWrapper({ results }) {
 
     useEffect(() => {
         if (!editorContent.id && firstResult) setEditorContent(results[0])
-        if (!results.length && editorContent.id) setEditorContent({});
+        if (!results.length && editorContent.id) setEditorContent(cleanEditorContent);
     }, [firstResult, editorContent, results])
 
-    console.log(results);
-
     return (
-        <>
-            {
-                results.map(
-                    (r, n) => {
-                        return (
-                            <button
-                                disabled={editorContent.title === r.title}
-                                onClick={() => setEditorContent(r)}
-                                key={n}
-                            >
-                                {r.title}
-                            </button>
-                        )
-                    }
-                )
-            }
-            <Editor
-                height="80vh"
-                theme="vs-dark"
-                path={editorContent.title || ""}
-                defaultLanguage={editorContent.inputType || "json"}
-                defaultValue={typeof editorContent.value === "object" ? JSON.stringify(editorContent.value, null, 2) : editorContent.value || ""}
-                onMount={(editor) => (editorRef.current = editor)}
-            />
-        </>
-    )
+      <>
+        <button
+          className="result-button"
+          onClick={() => {
+            const a = document.createElement("a");
+            a.download = `myResult.${
+              typeof editorContent.value === "string" ? "txt" : "json"
+            }`;
+            const blob = new Blob(
+              [
+                typeof editorContent.value === "string"
+                  ? editorContent.value
+                  : JSON.stringify(editorContent.value, null, 2),
+              ],
+              { type: "application/json" }
+            );
+            a.href = URL.createObjectURL(blob);
+            a.addEventListener("click", (e) => {
+              setTimeout(() => URL.revokeObjectURL(a.href), 30 * 1000);
+            });
+            a.click();
+          }}
+        >
+          {"R>"}
+        </button>
+        {results.map((r, n) => {
+          return (
+            <button
+              disabled={editorContent.title === r.title}
+              onClick={() => setEditorContent(r)}
+              key={n}
+            >
+              {r.title}
+            </button>
+          );
+        })}
+        <Editor
+          height="80vh"
+          theme="vs-dark"
+          path={editorContent.title}
+          defaultLanguage={editorContent.inputType}
+          defaultValue={
+            typeof editorContent.value === "object"
+              ? JSON.stringify(editorContent.value, null, 2)
+              : editorContent.value
+          }
+          options={{ readOnly: true, domReadOnly: true }}
+          onMount={(editor) => (editorRef.current = editor)}
+        />
+      </>
+    );
 }
 
 export default EditorWrapper
